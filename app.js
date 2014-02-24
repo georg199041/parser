06a56b175b8ff2db9fdcf5f72840a31476137691
George <georgandco@gmail.com>
2014-02-24 22:29:53 +0200

    Parser+

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 var request = require('request');
 var htmlparser = require("htmlparser2");
 var fs = require('fs');
-var util = require('util');
 var json = require('format-json');
 var data = [];
-var test = {'test': 123, 'test2': 'trere'}
 
 request('http://makeup.com.ua/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-        var obj = {};
 
         var parser = new htmlparser.Parser({
             onopentag: function(name, attribs){
