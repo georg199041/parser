a0a98a217e9df2b0b3620b956140a6f195845101
George <georgandco@gmail.com>
2014-02-24 21:44:29 +0200

    parser+

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var request = require('request');
 var htmlparser = require("htmlparser2");
 var fs = require('fs');
 var util = require('util');
+var json = require('format-json');
 var data = [];
-
+var test = {'test': 123, 'test2': 'trere'}
 
 request('http://makeup.com.ua/', function (error, response, body) {
     if (!error && response.statusCode == 200) {
         var obj = {};
+
         var parser = new htmlparser.Parser({
             onopentag: function(name, attribs){
                 if (name == 'img') {
@@ -19,12 +21,13 @@ request('http://makeup.com.ua/', function (error, response, body) {
             },
             ontext: function(text){
                 //console.log(text)
+
             },
             onclosetag: function(tagname){
                // data.push()
             },
             onend: function() {
-                fs.writeFile('./test.json', JSON.stringify(util.inspect(data, { showHidden: true, depth: 1 }), null, 4), function(err) {
+                fs.writeFile('./test.json', json.diffy(data), function(err) {
                     if (err) throw err;
                 });
             }
